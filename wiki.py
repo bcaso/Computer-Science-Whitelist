e25d560562033dedfdb13acec6337fde27f4e739
bcaso <bcaso@qq.com>
2021-11-13 07:47:16 +0800

    add gitbooks.io

--- wiki.py
+++ wiki.py
@@ -1,5 +1,6 @@
 Whitelist = {'wikipedia':['zh','org'], # 中文维基
         'zhihu':['','com'],           # 知乎
         'runnoob':['','com'],          # 菜鸟教程
-        'python':['','org']           # python 官方, 文档
+        'python':['','org'],           # python 官方, 文档
+        'gitbooks':['','io'],
 } 
