7b66f655f4f79850bf8d09a0138a1a02726c629f
bcaso <bcaso@qq.com>
2021-11-13 16:01:38 +0800

    add askubuntu,medium,github.community

--- wiki.py
+++ wiki.py
@@ -2,5 +2,5 @@ Whitelist = {'wikipedia':['zh','org'], # 中文维基
         'zhihu':['','com'],           # 知乎
         'runnoob':['','com'],          # 菜鸟教程
         'python':['','org'],           # python 官方, 文档
-        'gitbooks':['','io'],
+        'gitbooks':['','io'],          # gitbook
 } 
--- 论坛.py
+++ 论坛.py
@@ -1,7 +1,10 @@
 Whitelist = {
         'StackOverflow':['','com'],
+        'askubuntu':['','com'],     # about linux
         'segmentfault':['','com'],
         'autoahk':['','com'],       # AHK 脚本
         'v2ex':['','com'],
         '52pojie':['','cn'],        # 吾爱
+        'github':['','community'],  # github 社区
+        'medium':['','com'],
 }
