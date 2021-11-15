e311111c17839106a62be9460790e7ab0e004473
bcaso <bcaso@qq.com>
2021-11-15 11:36:56 +0800

    add wikihow.com

--- wiki.py
+++ wiki.py
@@ -1,6 +1,7 @@
 Whitelist = {'wikipedia':['zh','org'], # 中文维基
-        'zhihu':['','com'],           # 知乎
+        'zhihu':['','com'],            # 知乎
         'runnoob':['','com'],          # 菜鸟教程
         'python':['','org'],           # python 官方, 文档
         'gitbooks':['','io'],          # gitbook
-} 
+        'wikihow':['zh','com'],        # wikihow, about health
+}
