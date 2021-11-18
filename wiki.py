052e3fcda085bc6cc9b216e6b7ed5e15c35c5db2
bcaso <bcaso@qq.com>
2021-11-18 10:04:54 +0800

    add shuxuele.com

--- wiki.py
+++ wiki.py
@@ -1,7 +1,8 @@
 Whitelist = {'wikipedia':['zh','org'], # 中文维基
         'zhihu':['','com'],            # 知乎
-        'runoob':['','com'],          # 菜鸟教程
+        'runoob':['','com'],           # 菜鸟教程
         'python':['','org'],           # python 官方, 文档
         'gitbooks':['','io'],          # gitbook
         'wikihow':['zh','com'],        # wikihow, about health
+        'shuxuele':['','com'],         # 数学乐
 }
