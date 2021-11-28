1a76053d4cdb7cdc2a7e70c4c2b4004c567566e2
bcaso <bcaso@qq.com>
2021-11-28 22:35:45 +0800

    add quora.com

--- wiki.py
+++ wiki.py
@@ -1,4 +1,5 @@
 Whitelist = {'wikipedia':['zh','org'], # 中文维基
+        'quora':['','com'],            # quora 类似知乎
         'zhihu':['','com'],            # 知乎
         'runoob':['','com'],           # 菜鸟教程
         'python':['','org'],           # python 官方, 文档
