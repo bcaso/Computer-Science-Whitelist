08f65e0e7139bfec2984e2a1372e51bbda97259c
bcaso <bcaso@qq.com>
2021-11-30 23:32:30 +0800

    change zh.wikipedia to *.wikipedia, add mathsisfun.com

--- wiki.py
+++ wiki.py
@@ -1,4 +1,4 @@
-Whitelist = {'wikipedia':['zh','org'], # 中文维基
+Whitelist = {'wikipedia':['','org'],   # 维基
         'quora':['','com'],            # quora 类似知乎
         'zhihu':['','com'],            # 知乎
         'runoob':['','com'],           # 菜鸟教程
@@ -6,5 +6,6 @@ Whitelist = {'wikipedia':['zh','org'], # 中文维基
         'gitbooks':['','io'],          # gitbook
         'wikihow':['zh','com'],        # wikihow, about health
         'shuxuele':['','com'],         # 数学乐
+        'mathsisfun':['','com'],       # 数学乐, 英文版
         'math001':['','com'],          # 数学网站
 }
