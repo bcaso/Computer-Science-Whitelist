861c92d5edf11fc7e477b70332d4e7b0345d1503
bcaso <bcaso@qq.com>
2021-12-28 22:17:04 +0800

    add c.biancheng.net

--- wiki.py
+++ wiki.py
@@ -2,6 +2,7 @@ Whitelist = {'wikipedia':['','org'],   # 维基
         'quora':['','com'],            # quora 类似知乎
         'zhihu':['','com'],            # 知乎
         'runoob':['','com'],           # 菜鸟教程
+        'biancheng':['c','net'],       # C 语言中文网，各方面的编程文档都有
         'python':['','org'],           # python 官方, 文档
         'gitbooks':['','io'],          # gitbook
         'wikihow':['zh','com'],        # wikihow, about health
