fec9e67794209e7f09f998609778489eb34072df
bcaso <bcaso@qq.com>
2022-01-01 14:40:38 +0800

    过滤 “园荐 recomm.cnblogs.com”

--- 博客.py
+++ 博客.py
@@ -1,7 +1,7 @@
 Whitelist = {
         'gitee':['','io'],
         'github': ['','io'],          # 没法 用 @://github.*/* 来表示 .com 和 .io，因为这条规则是有问题的
-        'cnblogs':['','com'],         # 博客园
+        'cnblogs':['www','com'],      # 博客园，加上 www 是为了过滤 https://recomm.cnblogs.com/ 这个地址
         'liaoxuefeng':['','com'],     # 廖雪峰的官方网站
         'ruanyifeng':['','com'],      # 阮一峰的网络日志
 }
