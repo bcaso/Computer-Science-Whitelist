86333cf8ce4fa34e67bdf7b0c711b19c287bb412
bcaso <bcaso@qq.com>
2022-01-02 09:52:12 +0800

    add oschina

--- 论坛.py
+++ 论坛.py
@@ -1,15 +1,16 @@
 Whitelist = {
         'stackoverflow':['','com'],
-        'superuser':['','com'],     # 类似 stackoverflow
-        'stackexchange':['','com'], # stack exchange
-        'serverfault':['','com'],   # stack exchange 下关于网络的子站
-        'askubuntu':['','com'],     # about linux
-        'segmentfault':['','com'],
-        'autoahk':['','com'],       # AHK 脚本
-        'v2ex':['','com'],
-        'appinn':['meta','net'],    # 小众论坛
-        '52pojie':['','cn'],        # 吾爱
-        'github':['','community'],  # github 社区
-        'nowcoder':['','com/questionTerminal'],  # 牛客网试题广场
+        'superuser':['','com'],          # 类似 stackoverflow
+        'stackexchange':['','com'],      # stack exchange
+        'serverfault':['','com'],        # stack exchange 下关于网络的子站
+        'askubuntu':['','com'],          # about linux
+        'segmentfault':['','com'],       
+        'autoahk':['','com'],            # AHK 脚本
+        'v2ex':['','com'],               # v2ex社区，经常无法打开
+        'appinn':['meta','net'],         # 小众论坛
+        'oschina':['','net/question'],   # 开源中国问答区
+        '52pojie':['','cn'],             # 吾爱论坛
+        'github':['','community'],       # github 社区
+        'nowcoder':['','com/questionTerminal'],  # 牛客网试题问答区
         'medium':['','com'],
 }
