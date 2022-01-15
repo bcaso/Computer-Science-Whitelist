6474284eecd3dfbbd1f842a4c58095f0c80f405c
bcaso <bcaso@qq.com>
2022-01-15 10:19:42 +0800

    Add the domain name with the edu suffix

--- uWhitelist_subscription.py
+++ uWhitelist_subscription.py
@@ -12,6 +12,15 @@ lis_total = []
 # 所以将所有的规则改为 @*://*.前缀.域名.后缀/*
 # 如果前缀是 www. 就不要写在 Whitelist 的 value[0] 中 
 
+'''
+with prefix
+@*://*.prefix.domain_name.suffix/*
+no prefix
+@*://*.domain_name.suffix/*
+no prefix and no domain name, only the suffix
+@*://*.suffix/*
+'''
+
 def gen_urls(whitelist_dic):
     for k,v in whitelist_dic.items(): 
         url = '@*://*.'
@@ -19,12 +28,16 @@ def gen_urls(whitelist_dic):
         if v[0] != '':
             url+=v[0] + '.'
         # 加域名
-        url += k.lower() # uBlacklist 对域名区分大小写 @*://*.stackoverflow.com/* 与 @*://*.StackOverflow.com/* 拦截效果不同
+        if k != '':
+            url += k.lower() # uBlacklist 对域名区分大小写 @*://*.stackoverflow.com/* 与 @*://*.StackOverflow.com/* 拦截效果不同
+        else:
+            url = url[:-1]   # change url('@*://*.') to '@*://*', 为了添加指定后缀的域名，如 @*://*.edu
+
         # 后缀
         if v[1] != '':    
             url+='.'+v[1]+'/*'
 
-        #print(url) #    # @*://*.name.com/* 
+        #print(url)
         lis.append(url)
         lis_total.append(url)
 
