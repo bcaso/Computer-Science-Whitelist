b514122adef13272e117fea83f934129289545ea
bcaso <bcaso@qq.com>
2022-01-16 15:18:05 +0800

    add kb.cnblogs.com/page

--- wiki.py
+++ wiki.py
@@ -1,6 +1,7 @@
 Whitelist = {'wikipedia':['','org'],             # 维基
         'runoob':['','com'],                     # 菜鸟教程
         'biancheng':['c','net'],                 # C 语言中文网，各方面的编程文档都有
+        'cnblogs':['kb','com/page'],             # cnblogs 博客园知识库
         'python':['','org'],                     # python 官方, 文档
         'cisco':['','com/c/zh_cn/support/docs'], # cisco 中文文档
         'gitbooks':['','io'],                    # gitbook
