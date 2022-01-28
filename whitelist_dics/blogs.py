f324453529e49a77844f423640ad0768da19abd2
bcaso <bcaso@qq.com>
2022-01-28 14:37:40 +0800

    remove *.edu/*

--- whitelist_dics/blogs.py
+++ whitelist_dics/blogs.py
@@ -1,7 +1,6 @@
 Whitelist = {
         'gitee':['','io'],
         'github': ['','io'],          # 没法 用 @://github.*/* 来表示 .com 和 .io，因为这条规则是有问题的
-        '': ['','edu'],               # 所有以 edu 结尾的域名(学校)
         'cnblogs':['www','com'],      # 博客园，加上 www 是为了过滤 https://recomm.cnblogs.com/ 这个地址
         'oschina':['my','net'],       # 开源中国博客
         'zhihu':['zhuanlan','com'],   # 知乎专栏
