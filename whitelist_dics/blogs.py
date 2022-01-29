f22fe210c040f700619edbe945f4b70029e71090
bcaso <bcaso@qq.com>
2022-01-29 10:16:01 +0800

    remove "my.oschina.net" from blogs.Whitelist

--- whitelist_dics/blogs.py
+++ whitelist_dics/blogs.py
@@ -2,7 +2,6 @@ Whitelist = {
         'gitee':['','io'],
         'github': ['','io'],          # 没法 用 @://github.*/* 来表示 .com 和 .io，因为这条规则是有问题的
         'cnblogs':['www','com'],      # 博客园，加上 www 是为了过滤 https://recomm.cnblogs.com/ 这个地址
-        'oschina':['my','net'],       # 开源中国博客
         'zhihu':['zhuanlan','com'],   # 知乎专栏
         'liaoxuefeng':['','com/wiki'],# 廖雪峰的官方网站
         'ruanyifeng':['','com'],      # 阮一峰的网络日志
---
+++ whitelist_dics/blogs_blacklist.py
@@ -0,0 +1,4 @@
+Blacklist = {
+    'oschina':['my','net'],          # 开源中国博客, 总是遇到访问受限, 博客无法访问，且没有踩的按钮。
+    'segmentfault':['','com/a'],     # 思否，国内版 stackoverflow, 博客文章不能踩，所以不添加到博客列表中
+}
