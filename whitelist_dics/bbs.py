799a0c8b24400307e7a913d2b7efc44bc4134451
bcaso <bcaso@qq.com>
2022-01-29 19:01:14 +0800

    merge again

--- whitelist_dics/bbs.py
+++ whitelist_dics/bbs.py
@@ -1,27 +1,4 @@
 Whitelist = {
-<<<<<<< HEAD
-        'medium':['','com'],
-        'stackoverflow':['','com'],
-        'superuser':['','com'],          # 类似 stackoverflow
-        'stackexchange':['','com'],      # stack exchange
-        'serverfault':['','com'],        # stack exchange 下关于网络的子站
-        'askubuntu':['','com'],          # about linux
-        'quora':['','com'],              # quora 类似知乎
-        'zhihu':['','com/question'],     # 知乎
-        'segmentfault':['','com/q'],     # 思否，国内版 stackoverflow, 博客文章不能踩，所以不添加到博客列表中
-        'autoahk':['','com/archives'],   # AutoHotkey 中文社区
-        'autohotkey':['','com/boards'],  # AutoHotkey 官方论坛
-        'v2ex':['','com'],               # v2ex社区，经常无法打开
-        'appinn':['meta','net'],         # 小众论坛
-        'oschina':['','net/question'],   # 开源中国问答区
-        '52pojie':['','cn'],             # 吾爱论坛
-        'cisco':['community','com'],        # cisco 社区
-        'github':['','community'],       # github 社区
-        'nowcoder':['','com/questionTerminal'],  # 牛客网试题问答区
-        'leetcode-cn':['','com/circle/discuss'], # 乐扣问答区
-        'letitfly':['bbs','me'],         # Android 刷机，现在也有电脑方面的内容
-        'ikuai8':['bbs','com'],          # iKuai 路由器论坛
-=======
         'medium':['','com','0.8'],
         'stackoverflow':['','com','0.8'],
         'superuser':['','com','0.8'],          # 类似 stackoverflow
@@ -43,6 +20,4 @@ Whitelist = {
         '52pojie':['','cn','0.4'],             # 吾爱论坛
         'letitfly':['bbs','me','0.4'],         # Android 刷机，现在也有电脑方面的内容
         'cisco':['community','com','0.3'],     # cisco 社区
->>>>>>> whitelists_1
-
 }
--- whitelist_dics/blogs.py
+++ whitelist_dics/blogs.py
@@ -1,14 +1,4 @@
 Whitelist = {
-<<<<<<< HEAD
-        'gitee':['','io'],
-        'github': ['','io'],          # 没法 用 @://github.*/* 来表示 .com 和 .io，因为这条规则是有问题的
-        'cnblogs':['www','com'],      # 博客园，加上 www 是为了过滤 https://recomm.cnblogs.com/ 这个地址
-        'zhihu':['zhuanlan','com'],   # 知乎专栏
-        'liaoxuefeng':['','com/wiki'],# 廖雪峰的官方网站
-        'ruanyifeng':['','com'],      # 阮一峰的网络日志
-        'coolshell':['','cn/articles'],        # 酷壳
-        'leetcode-cn':['','com/problems'], # 乐扣试题区，不好分类的网址
-=======
         'leetcode-cn':['','com/problems','0.8'],   # 乐扣试题区，不好分类的网址
         'github': ['','io','0.8'],                 # 没法 用 @://github.*/* 来表示 .com 和 .io，因为这条规则是有问题的
         'gitee':['','io','0.7'],
@@ -18,6 +8,4 @@ Whitelist = {
         'liaoxuefeng':['','com/wiki','0.5'],       # 廖雪峰的官方网站
         'coolshell':['','cn/articles','0.5'],      # 酷壳
         '': ['','edu','0.1'],                      # 所有以 edu 结尾的域名(学校)
->>>>>>> whitelists_1
-
 }
