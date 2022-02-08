31681b40880c5005f2336432cccc11eb1cde559c
bcaso <hm20160607@gmail.com>
2022-02-08 04:32:07 +0000

    add nowcoder.com/practice

--- whitelist_dics/blogs.py
+++ whitelist_dics/blogs.py
@@ -1,9 +1,10 @@
 Whitelist = {
         # Foreign science and technology blog posts 
         'medium':['','com','0.8'],
-        'groovypost':['','com/howto','0.8'],       # 
+        'groovypost':['','com/howto','0.8'],       # 电脑技术问题
 
-        'leetcode-cn':['','com/problems','0.8'],   # 乐扣试题区，不好分类的网址
+        'leetcode-cn':['','com/problems','0.8'],   # 乐扣试题区
+        'nowcoder':['','com/practice','0.8'],      # 牛客试题区
         'github': ['','io','0.8'],                 # 没法 用 @://github.*/* 来表示 .com 和 .io，因为这条规则是有问题的
         'gitee':['','io','0.7'],
         'zhihu':['zhuanlan','com','0.7'],          # 知乎专栏
