e11590d1d4bb40a10bf6063b98a46f354f5c18fe
bcaso <hm20160607@gmail.com>
2022-02-08 03:04:18 +0000

    add gitbook.io, china-ccie.com

--- whitelist_dics/library.py
+++ whitelist_dics/library.py
@@ -1,6 +1,6 @@
 Whitelist = {
-        'baidu':['wenku','com/view','0.7'],    # 百度文库
-        'docin':['','com/p-','0.7'],           # 豆丁文库
-        'doc88':['','com/p-','0.7'],           # 道客巴巴
-        'taodocs':['','com/p-','0.7'],         # 淘豆网
+        'baidu':['wenku','com/view','0.4'],    # 百度文库
+        'docin':['','com/p-','0.4'],           # 豆丁文库
+        'doc88':['','com/p-','0.4'],           # 道客巴巴
+        'taodocs':['','com/p-','0.4'],         # 淘豆网
 }
