73561ef459652a5e654967602cf90411c982c3cb
bcaso <hm20160607@gmail.com>
2022-02-12 19:45:06 +0800

    remove "kb.cnblogs.com", add "www.cnblogs.com"

--- whitelist_dics/repository.py
+++ whitelist_dics/repository.py
@@ -1,6 +1,7 @@
 Whitelist = {
-        'github': ['','com','0.8'],     # github 仓库
-        'pypi':['','org','0.75'],       # python 仓库
-        'gitee':['','com','0.7'],       # 码云
+        'github': ['','com','0.8'],          # github 仓库
+        'pypi':['','org','0.75'],            # python 仓库
+        'codeproject': ['','com','0.7'],     # github 仓库
+        'gitee':['','com','0.6'],            # 码云
 
 }
